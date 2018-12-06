fa017ab932192afad73e3df5aef55185031e5c31
mfdoom22 <nicolasdonoso048@gmail.com>
2018-12-06 01:00:26 -0300

    favicom

--- actualizarLibros.php
+++ actualizarLibros.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Libros</title>
+    <link rel=" icon"  href="images/ico.ico">
 </head>
 <body>
 <?php
